7f304d42741f52bcbc07be23d9a47a3a0f716d0e
Mateus-Alencar <mateus.alencar1@outlook.com>
2023-03-12 10:02:57 -0300

    first commit

---
+++ CalcularAreaTriangulo.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace exercicios
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CalcularAreaTriangulo : ContentPage
+    {
+        public CalcularAreaTriangulo()
+        {
+            InitializeComponent();
+        }
+
+        private void BtExecutar_Clicked(object sender, EventArgs e)
+        {
+            Double vBase = Convert.ToDouble(eBase.Text);
+            Double vAltura = Convert.ToDouble(eAltura.Text);
+            Double total = (vBase * vAltura) / 2;
+            lTotal.Text = "A área do triângulo é:" + total.ToString();
+        }
+    }
+}
